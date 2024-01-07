0d0bbed51752e360e020761b89c44549998c5ad9
Nicasiomarques <nicasiomarques18@gmail.com>
2024-01-07 16:25:32 +0100

    chore: add milliparsec as body parser

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
 import { App } from "@tinyhttp/app";
 import { cors } from "@tinyhttp/cors";
+import { json } from "milliparsec";
 
 import { Endpoint } from "@/constants";
 import { getCacheOrSet } from "@/infra";
-import { getPhotoAlbum } from "@/useCases";
+import { getPhotoOrAlbum } from "@/useCases";
 
 const app = new App();
 app.use(cors());
+app.use(json());
 
 app.get(Endpoint.Photos, async (request, response) => {
   const albumId = request.query.albumId as string;
-  const photos = await getCacheOrSet(`photos?${albumId}`, () => getPhotoAlbum({ albumId }));
+  const photos = await getCacheOrSet(`photos?${albumId}`, () => getPhotoOrAlbum({ albumId }));
   return response.json(photos);
 });
 
 app.get(`${Endpoint.Photos}/:photoId`, async ({ params: { photoId } }, response) => {
-  const photos = await getCacheOrSet(`photos:${photoId}`, async () => getPhotoAlbum({ photoId }));
+  const photos = await getCacheOrSet(`photos:${photoId}`, () => getPhotoOrAlbum({ photoId }));
   return response.json(photos);
 });
 
